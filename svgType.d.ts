84d940aec2e5b65410b36c68a591f8bb7824b6b6
Andrej Lacko <lacko.andy@gmail.com>
2022-04-26 15:54:47 +0200

    smarter typing

--- svgType.d.ts
+++ svgType.d.ts
@@ -1,10 +1,11 @@
 declare module '*.svg' {
-    /**
-     * Use `any` to avoid conflicts with
-     * `@svgr/webpack` plugin or
-     * `babel-plugin-inline-react-svg` plugin.
-     */
-    const content: any;
+  import type {ReactElement} from 'react'
+  /**
+   * Use `any` to avoid conflicts with
+   * `@svgr/webpack` plugin or
+   * `babel-plugin-inline-react-svg` plugin.
+   */
+  const content: ReactElement<any>
 
-    export default content;
+  export default content
 }
