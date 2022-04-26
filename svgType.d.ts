cb079b4a3068bbfdb0e658ddb287db2794a89bae
Andrej Lacko <lacko.andy@gmail.com>
2022-04-26 15:58:41 +0200

    mistyped

--- svgType.d.ts
+++ svgType.d.ts
@@ -1,11 +1,11 @@
 declare module '*.svg' {
-  import type {ReactElement} from 'react'
+  import type {ElementType} from 'react'
   /**
    * Use `any` to avoid conflicts with
    * `@svgr/webpack` plugin or
    * `babel-plugin-inline-react-svg` plugin.
    */
-  const content: ReactElement<any>
+  const content: ElementType<any>
 
   export default content
 }
