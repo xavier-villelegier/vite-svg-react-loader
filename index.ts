7490b3d5e1586992b288ab94b210e5b1f4b8efd6
Xavier Villelégier <x.villelegier@gmail.com>
2023-02-07 10:27:20 +0100

    add query option

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
-import {transform as transformSvgr} from '@svgr/core'
-import {transform} from 'esbuild'
+import { transform as transformSvgr } from '@svgr/core'
+import { transform } from 'esbuild'
 import fs from 'fs'
-import type {Plugin} from 'vite'
+import type { Plugin } from 'vite'
 
 interface SvgrPluginOptions {
+  query?: string
   keepEmittedAssets?: boolean
   svgrOptions?: SVGROptions
 }
@@ -27,32 +28,30 @@ export default function svgrPlugin(options: SvgrPluginOptions = {}): Plugin {
     name: 'vite:svgr',
 
     async transform(code: any, id: string) {
-      if (id.indexOf('.svg') === -1) {
+      const svgFilePattern = options?.query ? `.svg?${options.query}` : '.svg'
+      if (id.indexOf(svgFilePattern) === -1) {
         return null
       }
 
       const svgrOptions = options?.svgrOptions ?? {}
-      const svgDataPath = id
+      const svgName = options?.query ? id.split(`?${options.query}`)[0] : id
+      const svgDataPath = svgName
       const svgData = await fs.promises.readFile(svgDataPath, 'utf8')
-      const componentCode = await transformSvgr(svgData, svgrOptions, {filePath: svgDataPath})
-      const component = await transform(componentCode, {loader: 'jsx'})
-      transformed.push(`${id}?component`)
+      const componentCode = await transformSvgr(svgData, svgrOptions, { filePath: svgDataPath })
+      const component = await transform(componentCode, { loader: 'jsx' })
+      transformed.push(`${svgName}?component`)
 
-      return {code: component.code, map: null}
+      return { code: component.code, map: null }
     },
 
-    generateBundle(config: any, bundle: {[key: string]: any}) {
+    generateBundle(config: any, bundle: { [key: string]: any }) {
       if (options.keepEmittedAssets) {
         return
       }
       // Discard transformed SVG assets from bundle so they are not emitted
       for (const [key, bundleEntry] of Object.entries(bundle)) {
-        const {type, name} = bundleEntry
-        if (
-          type === 'asset' &&
-          name?.endsWith('.svg') &&
-          transformed.findIndex(id => id.includes(name)) >= 0
-        ) {
+        const { type, name } = bundleEntry
+        if (type === 'asset' && name?.endsWith('.svg') && transformed.findIndex((id) => id.includes(name)) >= 0) {
           delete bundle[key]
         }
       }
